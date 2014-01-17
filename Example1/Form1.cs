9abe9eb4772143cc6e464085d7efb254dc8a9cb1
Ed <ww2008a@gmail.com>
2014-01-17 10:32:20 -0500

    Example1 added

---
+++ Example1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using Examples.Extensions;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        private bool timedout_;
+        private DateTime timelimit_;
+        private static readonly string[] scripts_ =
+        {
+            "Good.bas",
+            "ParseError.bas",
+            "RuntimeError.bas",
+            "Stop.bas",
+            "TooLong.bas"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            ListBoxScripts_Initialize();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize Language with this as the IHost
+            ScriptingLanguage.SetHost(this);
+        }
+
+        private void buttonRunScript_Click(object sender, EventArgs e)
+        {
+            TheIncident = new Incident();
+            using (var basicNoUIObj = new WinWrap.Basic.BasicNoUIObj())
+            {
+                basicNoUIObj.Begin += basicNoUIObj_Begin;
+                basicNoUIObj.DoEvents += basicNoUIObj_DoEvents;
+                basicNoUIObj.ErrorAlert += basicNoUIObj__ErrorAlert;
+                basicNoUIObj.Pause_ += basicNoUIObj__Pause_;
+                basicNoUIObj.Secret = new System.Guid("00000000-0000-0000-0000-000000000000");
+                basicNoUIObj.Initialize();
+                /// Extend WinWrap Basic scripts with Example1.Extensions assembly
+                /// Add "Imports Examples.Extensions" to all WinWrap Basic scripts
+                /// Add "Imports Examples.Extensions.ScriptingLanguage" all WinWrap Basic scripts
+                basicNoUIObj.AddScriptableReference(typeof(ScriptingLanguage).Assembly,
+                    "Examples.Extensions Examples.Extensions.ScriptingLanguage");
+
+                if (!basicNoUIObj.LoadModule(ScriptPath("Globals.bas")))
+                    LogError(basicNoUIObj.Error);
+                else
+                {
+                    using (var module = basicNoUIObj.ModuleInstance(ScriptPath(Script), false))
+                    {
+                        if (module == null)
+                            LogError(basicNoUIObj.Error);
+                        else
+                        {
+                            // Execute script code via an event
+                            ScriptingLanguage.TheIncident.Start(this.Text);
+                        }
+                    }
+                }
+            }
+
+            TheIncident = null;
+        }
+
+        void basicNoUIObj_Begin(object sender, EventArgs e)
+        {
+            timedout_ = false;
+            timelimit_ = DateTime.Now + new TimeSpan(0, 0, 1);
+        }
+
+        void basicNoUIObj_DoEvents(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= timelimit_)
+            {
+                timedout_ = true;
+                WinWrap.Basic.BasicNoUIObj basicNoUIObj = sender as WinWrap.Basic.BasicNoUIObj;
+                // time timelimit has been reached, terminate the script
+                basicNoUIObj.Run = false;
+            }
+        }
+
+        void basicNoUIObj__ErrorAlert(object sender, EventArgs e)
+        {
+            WinWrap.Basic.BasicNoUIObj basicNoUIObj = sender as WinWrap.Basic.BasicNoUIObj;
+            LogError(basicNoUIObj.Error);
+        }
+
+        void basicNoUIObj__Pause_(object sender, EventArgs e)
+        {
+            WinWrap.Basic.BasicNoUIObj basicNoUIObj = sender as WinWrap.Basic.BasicNoUIObj;
+            // Script execution has paused, terminate the script
+            if (basicNoUIObj.Error == null)
+            {
+                LogError(Examples.SharedSource.WinWrapBasic.FormatTimeoutError(basicNoUIObj, timedout_));
+            }
+            // Script execution has paused, terminate the script
+            basicNoUIObj.Run = false;
+        }
+
+        private void listBoxScripts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxScript.Text = File.ReadAllText(ScriptPath(Script));
+        }
+    }
+}
---
+++ Examples.SharedSource/Form1_Partial_AZW.cs
@@ -0,0 +1,57 @@
+﻿using Examples.Extensions;
+using System;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Collections.Generic;
+
+namespace Example
+{
+    public partial class Form1
+    {
+        #region "IHost"
+
+        public Incident TheIncident { get; private set; }
+
+        public void Log(string text)
+        {
+            if (text.Length > 0)
+                Session["text"] = text.Trim() + Environment.NewLine + Environment.NewLine + (string)Session["text"];
+            LabelLog.Text = (string)Session["text"];
+            //LabelScript.Text = Script;
+            LabelCode.Text = File.ReadAllText(ScriptPath(Script));
+        }
+
+        #endregion
+
+        private string Script { get { return scripts_[ListBoxScripts.SelectedIndex]; } }
+
+        private void ListBoxScripts_Initialize()
+        {
+            foreach (string script in scripts_)
+                ListBoxScripts.Items.Add(script);
+            ListBoxScripts.SelectedIndex = 0;
+        }
+
+        private string ScriptPath(string script)
+        {
+            string dir = AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + @"\Scripts";
+            string path = string.Format(@"{0}\{1}", dir, script);
+            // Visual Studio debugger does not deploy App_Data dir contents
+            if (!File.Exists(path))
+                path = string.Format(@"{0}\..\..\..\..\Scripts\{1}", dir, script);
+            return path;
+        }
+
+        private void LogError(WinWrap.Basic.Error error)
+        {
+            Log(Examples.SharedSource.WinWrapBasic.FormatError(error));
+        }
+
+        private void LogError(string text)
+        {
+            Log(text);
+        }
+    }
+}
