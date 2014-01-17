6c2b27833390b686975a279f30bcc6900222869b
Ed <ww2008a@gmail.com>
2014-01-17 11:29:24 -0500

    Example8 added

---
+++ Example8/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelControls = new System.Windows.Forms.Panel();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBoxScripts = new System.Windows.Forms.ListBox();
+            this.buttonRunScript = new System.Windows.Forms.Button();
+            this.textBoxScript = new System.Windows.Forms.TextBox();
+            this.panelControls.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControls
+            // 
+            this.panelControls.Controls.Add(this.richTextBoxOutput);
+            this.panelControls.Controls.Add(this.panel1);
+            this.panelControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControls.Location = new System.Drawing.Point(0, 0);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(512, 140);
+            this.panelControls.TabIndex = 0;
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxOutput.Location = new System.Drawing.Point(164, 0);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(348, 140);
+            this.richTextBoxOutput.TabIndex = 1;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBoxScripts);
+            this.panel1.Controls.Add(this.buttonRunScript);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 140);
+            this.panel1.TabIndex = 0;
+            // 
+            // listBoxScripts
+            // 
+            this.listBoxScripts.FormattingEnabled = true;
+            this.listBoxScripts.Location = new System.Drawing.Point(9, 32);
+            this.listBoxScripts.Name = "listBoxScripts";
+            this.listBoxScripts.Size = new System.Drawing.Size(146, 95);
+            this.listBoxScripts.TabIndex = 1;
+            this.listBoxScripts.SelectedIndexChanged += new System.EventHandler(this.listBoxScripts_SelectedIndexChanged);
+            // 
+            // buttonRunScript
+            // 
+            this.buttonRunScript.Location = new System.Drawing.Point(9, 9);
+            this.buttonRunScript.Name = "buttonRunScript";
+            this.buttonRunScript.Size = new System.Drawing.Size(146, 19);
+            this.buttonRunScript.TabIndex = 0;
+            this.buttonRunScript.Text = "Run Script";
+            this.buttonRunScript.UseVisualStyleBackColor = true;
+            this.buttonRunScript.Click += new System.EventHandler(this.buttonRunScript_Click);
+            // 
+            // textBoxScript
+            // 
+            this.textBoxScript.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxScript.Location = new System.Drawing.Point(0, 140);
+            this.textBoxScript.Multiline = true;
+            this.textBoxScript.Name = "textBoxScript";
+            this.textBoxScript.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxScript.Size = new System.Drawing.Size(512, 282);
+            this.textBoxScript.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 422);
+            this.Controls.Add(this.textBoxScript);
+            this.Controls.Add(this.panelControls);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Example 8 - Lifespan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelControls.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelControls;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBoxScripts;
+        private System.Windows.Forms.Button buttonRunScript;
+        private System.Windows.Forms.TextBox textBoxScript;
+
+    }
+}
+
---
+++ Example8/Form1.cs
@@ -0,0 +1,166 @@
+﻿using Examples.ExtensionsImplements;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        private WinWrap.Basic.BasicNoUIObj basicNoUIObj;
+        private bool timedout_;
+        private DateTime timelimit_;
+        private Dictionary<string, InstanceAndTimestamp> mts = new Dictionary<string, InstanceAndTimestamp>();
+
+        struct InstanceAndTimestamp
+        {
+            public WinWrap.Basic.Instance instance;
+            public DateTime timestamp;
+        }
+
+        private static readonly string[] scripts_ =
+        {
+            "Good.cls",
+            "ParseError.cls",
+            "RuntimeError.cls",
+            "Stop.cls",
+            "TooLong.cls"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            ListBoxScripts_Initialize();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize Language with this as the IHost
+            ScriptingLanguage.SetHost(this);
+            // Create and initialize BasicNoUIObj object
+            basicNoUIObj = new WinWrap.Basic.BasicNoUIObj();
+            basicNoUIObj.Begin += basicNoUIObj_Begin;
+            basicNoUIObj.Disconnecting += basicNoUIObj_Disconnecting;
+            basicNoUIObj.DoEvents += basicNoUIObj_DoEvents;
+            basicNoUIObj.ErrorAlert += basicNoUIObj__ErrorAlert;
+            basicNoUIObj.Pause_ += basicNoUIObj__Pause_;
+            basicNoUIObj.Secret = new System.Guid("00000000-0000-0000-0000-000000000000");
+            basicNoUIObj.Initialize();
+            // automatically disconnect BasicNoUIObj when form closes
+            basicNoUIObj.AttachToForm(this, WinWrap.Basic.ManageConstants.All);
+            /* Extend WinWrap Basic scripts with Examples.Extensions assembly
+             * Add "Imports Examples.Extensions" to all WinWrap Basic scripts
+             * Add "Imports Examples.Extensions.ScriptingLanguage" all WinWrap Basic scripts */
+            basicNoUIObj.AddScriptableReference(typeof(ScriptingLanguage).Assembly,
+                "Examples.ExtensionsImplements Examples.Extensions.ScriptingLanguage");
+            if (!basicNoUIObj.LoadModule(ScriptPath("Globals.bas")))
+            {
+                LogError(basicNoUIObj.Error);
+                buttonRunScript.Enabled = false;
+            }
+        }
+
+        private void buttonRunScript_Click(object sender, EventArgs e)
+        {
+            TheIncident = new Incident();
+
+            InstanceAndTimestamp mt = new InstanceAndTimestamp();
+            string path = ScriptPath(Script);
+            DateTime timestamp = File.GetLastWriteTimeUtc(path);
+            if (mts.TryGetValue(path, out mt) && mt.timestamp != timestamp)
+            {
+                // file has changed, dump instance from cache
+                mts.Remove(path);
+                mt = new InstanceAndTimestamp();
+            }
+
+            try
+            {
+                if (mt.instance == null)
+                {
+                    // parse the script and create an instance
+                    mt.instance = basicNoUIObj.CreateInstance(path);
+                    if (mt.instance == null)
+                        LogError(basicNoUIObj.Error);
+                    else
+                    {
+                        // cache the instance with a timestamp
+                        mt.timestamp = File.GetLastWriteTimeUtc(path);
+                        mts.Add(path, mt);
+                    }
+                }
+
+                if (mt.instance != null)
+                {
+                    // Execute script code via an interface
+                    DateTime tstart2 = DateTime.Now;
+                    IIncidentAction action = mt.instance.Cast<IIncidentAction>();
+                    ScriptingLanguage.TheIncident.Start(action, "Form1");
+                    TimeSpan ts2 = DateTime.Now - tstart2;
+                    Debug.Print(ts2.ToString());
+                }
+            }
+            catch (WinWrap.Basic.TerminatedException)
+            {
+                // script execution terminated, ignore error
+            }
+            catch (Exception ex)
+            {
+                // script caused an exception
+                basicNoUIObj.ReportError(ex);
+            }
+
+            TheIncident = null;
+        }
+
+        void basicNoUIObj_Begin(object sender, EventArgs e)
+        {
+            timedout_ = false;
+            timelimit_ = DateTime.Now + new TimeSpan(0, 0, 1);
+        }
+
+        void basicNoUIObj_Disconnecting(object sender, EventArgs e)
+        {
+            // dispose of cached instances
+            foreach (InstanceAndTimestamp mt in mts.Values)
+                mt.instance.Dispose();
+
+            mts.Clear();
+        }
+
+        void basicNoUIObj_DoEvents(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= timelimit_)
+            {
+                timedout_ = true;
+                WinWrap.Basic.BasicNoUIObj basicNoUIObj = sender as WinWrap.Basic.BasicNoUIObj;
+                // time limit has been reached, terminate the script
+                basicNoUIObj.Run = false;
+            }
+        }
+
+        void basicNoUIObj__ErrorAlert(object sender, EventArgs e)
+        {
+            WinWrap.Basic.BasicNoUIObj basicNoUIObj = sender as WinWrap.Basic.BasicNoUIObj;
+            LogError(basicNoUIObj.Error);
+        }
+
+        void basicNoUIObj__Pause_(object sender, EventArgs e)
+        {
+            WinWrap.Basic.BasicNoUIObj basicNoUIObj = sender as WinWrap.Basic.BasicNoUIObj;
+            if (basicNoUIObj.Error == null)
+            {
+                LogError(Examples.SharedSource.WinWrapBasic.FormatTimeoutError(basicNoUIObj, timedout_));
+            }
+            // Script execution has paused, terminate the script
+            basicNoUIObj.Run = false;
+        }
+
+        private void listBoxScripts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxScript.Text = File.ReadAllText(ScriptPath(Script));
+        }
+    }
+}
